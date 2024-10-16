3ea542adbf327d5286345ab837ded5915b37ba18
unknown <a.cina@procreatech.com>
2024-10-16 11:54:05 +0200

    The Async Pipe - a Better way of passing Observable data to the View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,13 @@ export class AppComponent implements OnInit {
 
   // i corsi non li prendo più direttamente dal file db-data, ma li definerò tramite chiamata get http
   // courses = COURSES;
-  courses;
+  courses: Course[];
+
+  // ASYNC PIPE, pipe che ci permette di sottoscrivere un osservabile direttamente dal template, in modo da avere componenti più reattivi
+  // la variabile courses indicata come sopra è una variabile mutabile, non ha un tipo, potrebbe ricevere qualsiasi cosa
+  // per farla diventare immutabile la rinominiamo in courses$, il dollaro alla fine è una convenzione per indicare che si tratta di un osservable
+  // la tipizziamo come un Observable<Course[]> cioè un osservabile che restituisce un array di oggetti Course
+  courses$: Observable<Course[]>;
 
   // HTTP NG SERVICE, per fetchare dati da un DB
   // per utilizzare un servive va dichiarato un riferimento ad esso e NG saprà, quando istanzia questa classe, che deve fornire questa dipendenza
@@ -35,7 +41,7 @@ export class AppComponent implements OnInit {
   // questo Lyfecycle Hook viene chiamato dopo il costruttore, quindi la variabile http sarà disponibile
   ngOnInit() {
     // chiamata GET tramite http service, si passa l'url da fetchare, restituisce un observable
-    // quindi va effettuata un'iscrizione per ottenere i dati, tramite il metodo subscribe() che ci restituisce il valore restituito dalla chiamata GET
+    // quindi va effettuata un'iscrizione per ottenere i dati, tramite il metodo subscribe() che ci restituisce i dati contenuti nell'Observable restituito dalla chiamata GET
     // assegnamo il valore ricevuto alla variabile courses
     // this.http.get("/api/courses").subscribe((valore) => {
     //   console.log(valore);
@@ -51,9 +57,18 @@ export class AppComponent implements OnInit {
 
     // i parametri li passiamo in un oggetto come secondo argomento della chiamata GET, che ha diverse proprietà tra le quali params
     // se controllo in console l'url che viene chiamato, vedo che ci saranno i query params: http://localhost:63311/api/courses?page=1&pageSize=10
-    this.http.get("/api/courses", { params: params }).subscribe((valore) => {
-      console.log(valore);
-      this.courses = valore;
-    });
+    // se non tipizzo il get, cioè tipizzo cosa ricevo, il type di valore sarà Object e non potrei assegnarlo alla proprietà this.courses che è un array di Course
+    this.http
+      .get<Course[]>("/api/courses", { params: params })
+      .subscribe((valore) => {
+        console.log(valore);
+        this.courses = valore;
+      });
+
+    // per passare dati alla proprietà courses$, che è un osservabile, non devo più utilizzare il metodo subscribe()
+    // devo assegnarle il risultato della chiamata GET, chè è un osservable
+    this.courses$ = this.http.get<Course[]>("/api/courses", { params: params });
+    // la variabile courses$ è quindi un obeservable e la sottoscriveremo nel template tramite il pipe ASYNC
+    // quando si lavora recuperando dati da Observables è buona prassi utilizzare l'async pipe perchè questo si occuperà di annullare la sottoscrizione all'observable nel momento in cui un componente viene distrutto, utile per prevenire perdite di memoria
   }
 }
