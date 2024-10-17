f37c26e25f4001b52ddf2dbd2af62abd46559646
unknown <a.cina@procreatech.com>
2024-10-17 11:31:08 +0200

    64. Angular DI Host Decorator and Section Conclusion

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,48 +1,55 @@
-import {Directive, EventEmitter, HostBinding, HostListener, Input, Output} from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  Host,
+  HostBinding,
+  HostListener,
+  Input,
+  Output,
+} from "@angular/core";
+import { CoursesService } from "../services/courses.service";
 
 @Directive({
-    selector: '[highlighted]',
-    exportAs: 'hl'
+  selector: "[highlighted]",
+  exportAs: "hl",
 })
 export class HighlightedDirective {
+  @Input("highlighted")
+  isHighlighted = false;
 
-    @Input('highlighted')
-    isHighlighted = false;
+  @Output()
+  toggleHighlight = new EventEmitter();
 
-    @Output()
-    toggleHighlight = new EventEmitter();
+  // all'interno di una directive possiamo iniettare un service, ad esempio CoursesService
+  // iniettandolo così la direttiva utilizza l'istanza globale del service
+  // constructor(private coursesService: CoursesService) {
+  // se invece volessi utilizzare l'istanza relativa al service generato tramite provider locale del componente a cui è applicata la direttiva, devo utilizzare uno speciale decoratore @Host(), in questo modo NG va a cercare il provider nel componente a cui è applicata la direttiva (naturalmente se non c'è tramite registrazione nella proprietà providers del componente ottengo un errore che blocca l'esecuzione del codice)
+  // constructor(@Host() private coursesService: CoursesService) {
+  constructor() {
+    console.log("Directive created..");
+  }
 
-    constructor() {
+  @HostBinding("class.highlighted")
+  get cssClasses() {
+    return this.isHighlighted;
+  }
 
-        console.log('Directive created..');
+  @HostListener("mouseover", ["$event"])
+  mouseOver($event) {
+    console.log($event);
 
-    }
+    this.isHighlighted = true;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
 
-    @HostBinding('class.highlighted')
-    get cssClasses() {
-        return this.isHighlighted;
-    }
+  @HostListener("mouseleave")
+  mouseLeave() {
+    this.isHighlighted = false;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
 
-    @HostListener('mouseover', ['$event'])
-    mouseOver($event) {
-
-        console.log($event);
-
-        this.isHighlighted = true;
-        this.toggleHighlight.emit(this.isHighlighted);
-    }
-
-    @HostListener('mouseleave')
-    mouseLeave() {
-        this.isHighlighted = false;
-        this.toggleHighlight.emit(this.isHighlighted);
-    }
-
-    toggle() {
-        this.isHighlighted = !this.isHighlighted;
-        this.toggleHighlight.emit(this.isHighlighted);
-    }
-
-
-
+  toggle() {
+    this.isHighlighted = !this.isHighlighted;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
 }
